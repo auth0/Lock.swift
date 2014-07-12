841eff7adbbed2750112f51834fdcb816719b4e6
Hernan Zalazar <hernanzalazar@gmail.com>
2014-07-12 18:23:05 -0300

    Created Sign Up view class.

--- Pod/Classes/A0SignUpView.h
+++ Pod/Classes/A0SignUpView.h
@@ -8,6 +8,19 @@
 
 #import <UIKit/UIKit.h>
 
+typedef void(^A0SignUpBlock)(NSString *username, NSString *password);
+typedef void(^A0CancelBlock)();
+
 @interface A0SignUpView : UIView
+
+@property (copy, nonatomic) A0SignUpBlock signUpBlock;
+@property (copy, nonatomic) A0CancelBlock cancelBlock;
+
+@property (weak, nonatomic) IBOutlet UITextField *userTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UIButton *signUpButton;
+@property (weak, nonatomic) IBOutlet UIButton *cancelButton;
+
+- (void)hideKeyboard;
 
 @end
--- Pod/Classes/A0SignUpView.m
+++ Pod/Classes/A0SignUpView.m
@@ -8,24 +8,53 @@
 
 #import "A0SignUpView.h"
 
+#import "UIButton+A0SolidButton.h"
+
+@interface A0SignUpView ()
+
+@property (weak, nonatomic) IBOutlet UIView *userContainerView;
+@property (weak, nonatomic) IBOutlet UIView *passwordContainerView;
+
+- (IBAction)signUp:(id)sender;
+- (IBAction)cancel:(id)sender;
+- (IBAction)goToPasswordField:(id)sender;
+
+@end
+
 @implementation A0SignUpView
 
-- (id)initWithFrame:(CGRect)frame
-{
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    self.userContainerView.layer.borderWidth = 1.0f;
+    self.userContainerView.layer.borderColor = [[UIColor colorWithWhite:0.302 alpha:1.000] CGColor];
+    self.passwordContainerView.layer.borderWidth = 1.0f;
+    self.passwordContainerView.layer.borderColor = [[UIColor colorWithWhite:0.302 alpha:1.000] CGColor];
+
+    [self.signUpButton setBackgroundColor:[UIColor colorWithRed:0.086 green:0.129 blue:0.302 alpha:1.000] forState:UIControlStateNormal];
+    [self.signUpButton setBackgroundColor:[UIColor colorWithRed:0.043 green:0.063 blue:0.145 alpha:1.000] forState:UIControlStateHighlighted];
+    self.signUpButton.layer.cornerRadius = 5;
+    self.signUpButton.clipsToBounds = YES;
+}
+
+- (void)hideKeyboard {
+    [self.userTextField resignFirstResponder];
+    [self.passwordTextField resignFirstResponder];
+}
+
+- (void)signUp:(id)sender {
+    if (self.signUpBlock) {
+        self.signUpBlock(self.userTextField.text, self.passwordTextField.text);
     }
-    return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
-{
-    // Drawing code
+- (void)goToPasswordField:(id)sender {
+    [self.passwordTextField becomeFirstResponder];
 }
-*/
 
+- (void)cancel:(id)sender {
+    if (self.cancelBlock) {
+        self.cancelBlock();
+    }
+}
 @end
