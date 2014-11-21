4d3bd1767e0b5bd4aa4614050bce471c3ffcb9a7
Hernan Zalazar <hernanzalazar@gmail.com>
2014-11-21 12:52:06 -0300

    Create AWS sample project.

---
+++ Examples/AWS/AWS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AWS
+//
+//  Created by Hernan Zalazar on 11/21/14.
+//  Copyright (c) 2014 Auth0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
