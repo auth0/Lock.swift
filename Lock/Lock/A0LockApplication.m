0f7e22af4c916d4b8edbdba87a1a0156403815ef
Hernan Zalazar <hernanzalazar@gmail.com>
2015-05-18 20:43:26 -0300

    Use Lock-GooglePlus instead of subspec.

--- Lock/Lock/A0LockApplication.m
+++ Lock/Lock/A0LockApplication.m
@@ -25,6 +25,7 @@
 #import <Lock/Lock.h>
 #import <Lock-Facebook/A0FacebookAuthenticator.h>
 #import <Lock-Twitter/A0TwitterAuthenticator.h>
+#import <Lock-GooglePlus/A0GooglePlusAuthenticator.h>
 
 @implementation A0LockApplication
 
