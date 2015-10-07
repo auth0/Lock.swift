acd7f6f619400680786c5e48f28597a70ecc6dab
Hernan Zalazar <hernanzalazar@gmail.com>
2015-10-07 12:15:12 -0300

    Post notif when a universal-link is received

--- Pod/Classes/Email/A0EmailLockViewController.h
+++ Pod/Classes/Email/A0EmailLockViewController.h
@@ -57,11 +57,4 @@
  */
 @property (strong, nonatomic) A0AuthParameters *authenticationParameters;
 
-/**
- *  Block that returns a signed JWT with `create:users` scope for API v2. It's required to send SMS code.
- *  For more info: https://api.auth0.com/docs/api/v2
- *  @deprecated 1.14.0. Lock now use `/passwordless/start` endpoint instead of API v2.
- */
-@property (copy, nonatomic) NSString *(^auth0APIToken)() DEPRECATED_MSG_ATTRIBUTE("a API v2 JWT is no longer necessary");
-
 @end
