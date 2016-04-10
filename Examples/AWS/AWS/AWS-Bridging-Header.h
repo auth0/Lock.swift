65d2da96c7ea3818193d60b7141e525eeff4f726
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-04-10 12:16:34 -0300

    Remove Examples

--- Examples/AWS/AWS/AWS-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <Lock/Lock.h>
-#import <SimpleKeychain/A0SimpleKeychain.h>
-#import <AWSiOSSDKv2/S3.h>
-#import <AWSiOSSDKv2/AWSCore.h>
-#import <ISO8601DateFormatter/ISO8601DateFormatter.h>
