ce8fece5d3a9946c3d32d6eac9110a5ee03df794
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-06-21 22:03:23 -0700

    Basic project structure

---
+++ Lock/Lock.h
@@ -0,0 +1,19 @@
+//
+//  Lock.h
+//  Lock
+//
+//  Created by Hernan Zalazar on 6/21/16.
+//  Copyright © 2016 Auth0. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Lock.
+FOUNDATION_EXPORT double LockVersionNumber;
+
+//! Project version string for Lock.
+FOUNDATION_EXPORT const unsigned char LockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Lock/PublicHeader.h>
+
+
