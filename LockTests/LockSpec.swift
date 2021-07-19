b29d5db4a4b0d029b9c40adc8169126b882ee969
Rita Zerrizuela <zeta@widcket.com>
2021-07-19 13:55:47 -0300

    Expose onForgotPassword [SDK-2632] (#673)
    * Expose onForgotPassword
    
    * Add unit test for the public API method
    
    * Fix test

--- LockTests/LockSpec.swift
+++ LockTests/LockSpec.swift
@@ -176,6 +176,14 @@ class LockSpec: QuickSpec {
                 expect(executed) == true
             }
 
+            it("should register onForgotPassword callback") {
+                var email: String? = nil
+                let callback: (String) -> () = { email = $0 }
+                let _ = lock.onForgotPassword(callback: callback)
+                lock.observerStore.onForgotPassword("mail@mail.com")
+                expect(email) == "mail@mail.com"
+            }
+
             it("should register onPasswordless callback") {
                 var email: String? = nil
                 let callback: (String) -> () = { email = $0 }
